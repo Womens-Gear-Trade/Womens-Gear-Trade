7633b891b76ad7e2d3faa1f443eec6c34246285a
Ashley Hoath <ahoath87@gmail.com>
2023-03-24 10:42:51 -0600

    adjusted PORT

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk');
 const server = require('./app');
 // const app = require('app');
-const PORT = process.env['PORT'] ?? 8080;
+// const PORT = process.env['PORT'] ?? 8080;
 // const server = http.createServer(app);
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(
