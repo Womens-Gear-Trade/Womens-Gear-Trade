9a21f9b1d6fbe1a6a1a1951a012bf7b4d2e66cdf
Megan Miller <meganmiller485@gmail.com>
2023-04-13 11:23:26 -0600

    adjusted profile merge issue

--- Client/womens-gear-trade/src/Components/Profile.jsx
+++ Client/womens-gear-trade/src/Components/Profile.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import '../CSS/profile.css';
 import profPic from '../assets/AIProfPic.png';
 import { useAuth } from '../Custom-Hooks';
-import { Link } from 'react-router-dom';
 
 const Profile = () => {
   const user = useAuth();
@@ -19,7 +18,11 @@ const Profile = () => {
         <div>
           <div className='profileInfo-content'>
             <div>
-              <img id='profPic' src={profPic} alt='profPicHur'></img>
+              <img
+                id='profPic'
+                src={profPic}
+                alt='profPicHur'
+              ></img>
             </div>
             <div id='info-container'>
               <h3>{user.user.name}</h3>
@@ -31,9 +34,10 @@ const Profile = () => {
           </div>
           <div className='profileBio'>
             <h5>Bio:</h5>
-            <textarea id='textarea' value='Text'>
-              Tell the world about your interests!
-            </textarea>
+            <textarea
+              id='textarea'
+              value='Text'
+            ></textarea>
           </div>
           <hr id='profileHR'></hr>
           <div className='profileOptions'>
@@ -49,28 +53,7 @@ const Profile = () => {
             <div className='optionContainer'>ALL THE OPTIONS</div>
           </div>
         </div>
-
-      </div>
-      <div className='profileBio'>
-        <h5>Bio:</h5>
-        <textarea id='textarea'>Tell the world about your interests!</textarea>
-      </div>
-      <hr id='profileHR'></hr>
-      <div className='profileOptions'>
-        <nav id='profNav'>
-          <ul className='profileLinks'>
-            <li id='profLink'>Inbox</li>
-            <li id='profLink'>Selling</li>
-            <li id='profLink'>Buying</li>
-            <li id='profLink'>Saved</li>
-          </ul>
-          <div id='createButt'>
-            <Link to='/create'>Create Listing</Link>
-          </div>
-        </nav>
-        <div className='optionContainer'>ALL THE OPTIONS</div>
-      </div>
-
+      )}
     </div>
   );
 };
